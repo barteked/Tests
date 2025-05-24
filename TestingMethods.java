7a25c09be3d585661545efac9338505ce0fe8cda
Bartosz Kędra <bartekkedra@onet.pl>
2025-05-24 16:47:38 +0200

    Update TestingMethods.java

--- TestingMethods.java
+++ TestingMethods.java
@@ -1,42 +1,57 @@
-public class TestingMethods{
-@Test
-public void shouldReturn5WhenNumberIs1(){
- // given
- NumberChecker numberChecker = new NumberChecker();
- int number = 1;
- // when
- int actual = numberChecker.checkNumber(number);
- // then
- Assertions.assertEquals(5, actual);
-}
-@Test
-public void shouldReturn5WhenNumberIs2(){
- // given
- NumberChecker numberChecker = new NumberChecker();
- int number = 2;
- // when
- int actual = numberChecker.checkNumber(number);
- // then
- Assertions.assertEquals(5, actual);
-}
-@Test
-public void shouldReturn5WhenNumberIs3(){
- // given
- NumberChecker numberChecker = new NumberChecker();
- int number = 3;
- // when
- int actual = numberChecker.checkNumber(number);
- // then
- Assertions.assertEquals(5, actual);
-}
-@Test
-public void shouldReturn5WhenNumberIs4(){
- // given
- NumberChecker numberChecker = new NumberChecker();
- int number = 4;
- // when
- int actual = numberChecker.checkNumber(number);
- // then
- Assertions.assertEquals(5, actual);
-} 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestingMethods {
+
+    @Test
+    public void shouldReturn5WhenNumberIs1() {
+        // given
+        NumberChecker numberChecker = new NumberChecker();
+        int number = 1;
+
+        // when
+        int actual = numberChecker.checkNumber(number);
+
+        // then
+        Assertions.assertEquals(5, actual);
+    }
+
+    @Test
+    public void shouldReturn5WhenNumberIs2() {
+        // given
+        NumberChecker numberChecker = new NumberChecker();
+        int number = 2;
+
+        // when
+        int actual = numberChecker.checkNumber(number);
+
+        // then
+        Assertions.assertEquals(5, actual);
+    }
+
+    @Test
+    public void shouldReturn5WhenNumberIs3() {
+        // given
+        NumberChecker numberChecker = new NumberChecker();
+        int number = 3;
+
+        // when
+        int actual = numberChecker.checkNumber(number);
+
+        // then
+        Assertions.assertEquals(5, actual);
+    }
+
+    @Test
+    public void shouldReturn5WhenNumberIs4() {
+        // given
+        NumberChecker numberChecker = new NumberChecker();
+        int number = 4;
+
+        // when
+        int actual = numberChecker.checkNumber(number);
+
+        // then
+        Assertions.assertEquals(5, actual);
+    }
 }
