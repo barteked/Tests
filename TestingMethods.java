ef8c94871f95dcf148625ee709d28f378f516bb2
Bartosz Kędra <bartekkedra@onet.pl>
2025-05-24 14:41:53 +0000

    add exercise TestingMethods

---
+++ TestingMethods.java
@@ -0,0 +1,42 @@
+public class TestingMethods{
+@Test
+public void shouldReturn5WhenNumberIs1(){
+ // given
+ NumberChecker numberChecker = new NumberChecker();
+ int number = 1;
+ // when
+ int actual = numberChecker.checkNumber(number);
+ // then
+ Assertions.assertEquals(5, actual);
+}
+@Test
+public void shouldReturn5WhenNumberIs2(){
+ // given
+ NumberChecker numberChecker = new NumberChecker();
+ int number = 2;
+ // when
+ int actual = numberChecker.checkNumber(number);
+ // then
+ Assertions.assertEquals(5, actual);
+}
+@Test
+public void shouldReturn5WhenNumberIs3(){
+ // given
+ NumberChecker numberChecker = new NumberChecker();
+ int number = 3;
+ // when
+ int actual = numberChecker.checkNumber(number);
+ // then
+ Assertions.assertEquals(5, actual);
+}
+@Test
+public void shouldReturn5WhenNumberIs4(){
+ // given
+ NumberChecker numberChecker = new NumberChecker();
+ int number = 4;
+ // when
+ int actual = numberChecker.checkNumber(number);
+ // then
+ Assertions.assertEquals(5, actual);
+} 
+}
