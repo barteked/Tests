ed3717f34fa097ff3ccd139c29208e6d4a848e08
Bartosz Kędra <bartekkedra@onet.pl>
2025-05-24 17:22:27 +0200

    Create NumberChecker.java

---
+++ NumberChecker.java
@@ -0,0 +1,14 @@
+public class NumberChecker {
+
+    public int checkNumber(int number) {
+        if (number <= 0) {
+            return -1;
+        } else if (number == 0) {
+            return 0;
+        } else if (number < 5) {
+            return 5;
+        } else {
+            throw new IllegalStateException("bad number!");
+        }
+    }
+}
